1d0c31575e2da67e80dbda24f78693b932aacaf1
RustedTurnip <samuelwilcox@outlook.com>
2017-12-16 19:38:10 +0000

    Add identifiers header
    This commit adds the Identifiers.h file pre-emptively, containing some
    data that will be used later on, with focus currently on 'view' states.

---
+++ Identifiers.h
@@ -0,0 +1,40 @@
+/*!
+*	\file Identifiers.h
+*	\brief This file contains "Identifiers" e.g. enumerations, that are used across the program
+*
+*	\author Samuel A. Wilcox
+*	\date 16/12/2017
+*/
+#ifndef IDENTIFIERS_H
+#define IDENTIFIERS_H
+
+namespace Identifiers {
+
+	enum Continent {
+		NorthAmerica,
+		SouthAmerica,
+		Africa,
+		Europe,
+		Asia,
+		Australia,
+		ContinentError
+	};
+
+
+	/*!
+	*	Each enumeration, with the exception of DoNothing represents a state (or screen)
+	*	which will be used by the ViewManager to navigate to the correct screen, or enter
+	*	certain states
+	*/
+	enum StateChange {
+		DoNothing,
+		MainMenu,
+		PlayGame,
+		Settings,
+		Instructions,
+		Credits,
+		EndGame
+	};
+}
+
+#endif // !IDENTIFIERS_H
